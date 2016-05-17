786994c5a54c396a6968668b41eaf8aa45eb84d3
Greg Lu <greg.lu@gmail.com>
2016-05-17 13:34:51 -0400

    Updating cookbook to follow chef conventions

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,10 @@
-#api key attribute
-default[:Site24x7][:APIkey] = "YOUR_USER_API_KEY"
-default[:Site24x7][:proxy] = ""
+# api key attribute
+default['site24x7']['api_key'] = ""
+default['site24x7']['proxy'] = ""
 
-#package identification
-if node["kernel"]["machine"] == "i686" || node["kernel"]["machine"] == "i386"
-  default[:package] = "Site24x7_Linux_32bit.install"
+# package identification
+if node['kernel']['machine'] == 'i686' || node['kernel']['machine'] == 'i386'
+  default['site24x7']['package'] = 'Site24x7_Linux_32bit.install'
 else
-   default[:package] = "Site24x7_Linux_64bit.install"
+	default['site24x7']['package'] = 'Site24x7_Linux_64bit.install'
 end
-
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,33 +2,37 @@
 # Cookbook Name:: site24x7
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014
 #
 # All rights reserved - Do Not Redistribute
 #
 
-directory "/tmp/site24x7agent" do
-        mode 0755                             #mode 0755
-        action :create
-end
+if node['site24x7']['api_key'].empty?
+  Chef::Log.info('Site24x7 API key missing. Skipping installation...')
+else
+  directory '/tmp/site24x7agent' do
+    mode 0755
+    action :create
+  end
 
-site24x7 "get_package"  do
-  package "#{node[:package]}"
-  action :create
-end
+  site24x7 'get_package' do
+    package node['site24x7']['package']
+    action :create
+  end
 
-file "/tmp/site24x7agent/#{node[:package]}" do
-  mode 0755
-end
+  file "/tmp/site24x7agent/#{node['site24x7']['package']}" do
+    mode 0755
+  end
 
-site24x7 "install" do
-  key "#{node[:Site24x7][:APIkey]}"
-  package "#{node[:package]}"
-  proxy "#{node[:Site24x7][:proxy]}"
-  action :install
-end
+  site24x7 'install' do
+    key node['site24x7']['api_key']
+    package node['site24x7']['package']
+    proxy node['site24x7']['proxy']
+    action :install
+  end
 
-service "site24x7monagent" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
+  service 'site24x7monagent' do
+    supports :status => true, :restart => true, :reload => true
+    action [ :enable, :start ]
+  end
 end
